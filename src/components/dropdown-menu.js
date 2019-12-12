b89b46369e757e35309538b1b8403f6825cbc421
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-12 17:32:52 -0500

    Refactors dropdown menu component and revises dropdown css

--- src/components/dropdown-menu.js
+++ src/components/dropdown-menu.js
@@ -4,44 +4,52 @@
 */
 
 import React from "react"
+import { FaAngleUp, FaAngleDown } from "react-icons/fa"
 import { MenuBtn } from "./menu-btn"
+import dropdownMenuStyles from "./dropdown-menu.module.css"
 
 class DropdownMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isOpen: false };
+    this.state = { listOpen: false };
 
     // This binding is necessary to make `this` work in the callback
-    this.open = this.open.bind(this);
+    this.toggleList = this.toggleList.bind(this);
   }
 
-  open() {
+  toggleList() {
     this.setState(state => ({
-      isOpen: !state.isOpen
+      listOpen: !state.listOpen
     }));
   }
 
   render() {
-    if (this.state.isOpen) {
-      return (
-        <>
-          <button onClick={this.open}>
-            Menu
-          </button>
-          <ul>
-            <MenuBtn text='Home' />
-            <MenuBtn text='About' />
-            <MenuBtn text='Contact' />
-          </ul >
-        </>
+    const listOpen = this.state.listOpen;
+    let menu;
+    let icon;
+
+    if (listOpen) {
+      menu = (
+        <ul className={dropdownMenuStyles.list}>
+          <MenuBtn text='Home' />
+          <MenuBtn text='About' />
+          <MenuBtn text='Contact' />
+        </ul>
       );
+      icon = <FaAngleUp />;
     } else {
-      return (
-        <button onClick={this.open}>
-          Menu
-        </button>
-      );
+      menu = null;
+      icon = <FaAngleDown />;
     }
+
+    return (
+      <div className={dropdownMenuStyles.menu}>
+        <button className={dropdownMenuStyles.mainMenuBtn} onClick={this.toggleList}>
+          Menu {icon}
+        </button>
+        {menu}
+      </div>
+    );
   }
 }
 
