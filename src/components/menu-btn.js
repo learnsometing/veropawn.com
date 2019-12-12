f341e992310c6a8be8b72fb0798bed359057a9c4
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-12 17:40:16 -0500

    Imports and uses menuItem from dropdown menu css

--- src/components/menu-btn.js
+++ src/components/menu-btn.js
@@ -3,11 +3,12 @@
 */
 
 import React from "react";
+import dropdownMenuStyles from "./dropdown-menu.module.css"
 
 function MenuBtn({ text }) {
   return (
-    <li>
-      <button>
+    <li className={dropdownMenuStyles.menuItem}>
+      <button className={dropdownMenuStyles.menuItem}>
         {text}
       </button>
     </li>
