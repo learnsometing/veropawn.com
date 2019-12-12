10f61e232c7976bd2b43e6736555237ea7bda3ae
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-12 12:21:19 -0500

    Implements header logo component with basic layout styles

---
+++ src/components/header-logo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import headerLogoStyles from "./header-logo.module.css"
+import { Link } from "gatsby"
+
+const HeaderLogo = ({ logo }) => {
+  return (
+    <div class={headerLogoStyles.container}>
+      <Link to='/'>
+        <img class={headerLogoStyles.logo} src={logo.publicURL} alt="Cash Pawn and Jewelry Logo" />
+      </Link>
+    </div>
+  )
+}
+
+export { HeaderLogo }
