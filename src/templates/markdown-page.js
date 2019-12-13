c07544d5abeb57d4224636784958baab2f43f5fb
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-13 18:02:15 -0500

    Creates template for markdown pages

---
+++ src/templates/markdown-page.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+import { graphql } from "gatsby";
+
+import Layout from "../components/layout";
+
+export default ({ data }) => {
+  const createHtmlFromMarkup = () => {
+    const html = data.markdownRemark.html;
+    return ReactHtmlParser(html);
+  }
+
+  return (
+    <Layout>
+      <div>
+        {createHtmlFromMarkup()}
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($id: String) {
+    markdownRemark(id: {eq: $id}) {
+      html
+    }
+  }
+`
