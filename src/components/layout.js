ef0c769fab01b9e8a5acc823f6f346820c5c136d
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-17 16:58:32 -0500

    Removes page-2 and references to page-2 in index

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,6 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
+
 import Header from "./header"
 import "./layout.css"
 
@@ -18,7 +12,6 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
