440320fa7569b64acd7794e76ee9c508d3d2b1ed
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-16 20:17:50 -0500

    Removes div from dd-menu list and adds setDropdownMenuRef prop for handling clicks outside of the menu

--- src/components/dd-menu-list.js
+++ src/components/dd-menu-list.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ddMenuStyles from "./dropdown-menu.module.css";
 
-export default ({ children }) => {
+export default ({ children, setDropdownMenuRef }) => {
   return (
-    <ul className={ddMenuStyles.ulist}>
+    <ul
+      className={ddMenuStyles.ulist}
+      ref={setDropdownMenuRef}
+    >
       {children}
     </ul>
   );
