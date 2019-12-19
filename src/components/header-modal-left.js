7d902bfbc07c0a53c80b2ab6184ea2ae72adf952
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-18 20:12:39 -0500

    Rewrites HeaderModalLeft
    Handling clicks outside the dropdown menu in HeaderModalLeft was not working well,
    so the HeaderModalLeft component was rewritten using ReactModal from the react-modal
    package.

--- src/components/header-modal-left.js
+++ src/components/header-modal-left.js
@@ -1,80 +1,69 @@
 /*
 * Header Modal Left
 *
-* Modal for main menu dropdown content that appears on the left side of the screen.
+* Modal for header menu dropdown content that appears on the left side of the screen.
 * 
 * The modal renders either the main menu or browse menu content based on props.
 * 
-* Maintains a reference to the current dropdown menu, which
-* is set by setDropdownMenuRef dynamically. This reference is used to close the
-* modal if the user is clicks outside it.
 */
 
-import React from "react"
-import onClickOutside from "react-onclickoutside"
+import React from "react";
+import ReactModal from "react-modal";
 
 import CollapsedMainMenu from "./collapsed-main-menu";
-import BrowseByCategoryMenu from "./browse-category-menu"
+import BrowseByCategoryMenu from "./browse-category-menu";
 
-import headerModalLeftStyles from "./header-modal-left.module.css"
+// import headerModalLeftStyles from "./header-modal-left.module.css";
+ReactModal.setAppElement('#___gatsby');
 
-class HeaderModalLeft extends React.Component {
-  constructor(props) {
-    super(props);
-    //handling outside clicks
-    this.dropdownMenu = null;
-    this.setDropdownMenuRef = this.setDropdownMenuRef.bind(this)
-    this.state = {
-      isOpen: (this.props.mainMenuOpen || this.props.browseMenuOpen)
-    }
-  }
+export default (props) => {
+  const isOpen = props.isOpen;
+  const mainMenuOpen = props.mainMenuOpen;
+  const browseMenuOpen = props.browseMenuOpen;
 
-  setDropdownMenuRef = element => {
-    this.dropdownMenu = element;
-  };
+  let modal = null;
+  if (isOpen) {
+    let menu;
 
-  handleClickOutside(event) {
-    if (this.dropdownMenu && !this.dropdownMenu.contains(event.target)) {
-      this.setState(({
-        isOpen: false
-      }));
-    }
-  }
-
-  render() {
-    const mainMenuOpen = this.props.mainMenuOpen;
-    const browseMenuOpen = this.props.browseMenuOpen;
-    const isOpen = this.state.isOpen;
-
-    let modal = null;
-
-    if (isOpen) {
-      let menu;
-
-      if (mainMenuOpen) {
-        menu = <CollapsedMainMenu
-          id="main-menu"
-          openBrowseMenu={this.props.openBrowseMenu}
-          setDropdownMenuRef={this.props.setDropdownMenuRef}
-        />
-      } else if (browseMenuOpen) {
-        menu = <BrowseByCategoryMenu
-          closeBrowseMenu={this.props.closeBrowseMenu}
-          setDropdownMenuRef={this.props.setDropdownMenuRef}
-        />;
-      }
-
-      modal = <div className={headerModalLeftStyles.wrapper}>
-        <div className={headerModalLeftStyles.overlay}>
-        </div>
-        {menu}
-      </div>
-
+    if (mainMenuOpen) {
+      menu = <CollapsedMainMenu
+        id="main-menu"
+        openBrowseMenu={props.openBrowseMenu}
+      />
+    } else if (browseMenuOpen) {
+      menu = <BrowseByCategoryMenu
+        closeBrowseMenu={props.closeBrowseMenu}
+      />;
     }
 
-    return modal;
-
+    modal = <ReactModal
+      isOpen={isOpen}
+      onRequestClose={props.closeModal}
+      style={{
+        overlay: {
+          height: '100%',
+          width: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+          zIndex: 1
+        },
+        content: {
+          width: '85vw',
+          height: '100%',
+          position: 'fixed',
+          top: '48px',
+          left: 0,
+          margin: 0,
+          borderRadius: 0,
+          backgroundColor: '#fff',
+          zIndex: 1,
+          overflowY: 'auto',
+          overflowX: 'hidden'
+        }
+      }}
+    >
+      {menu}
+    </ReactModal >
   }
+
+  return modal;
 }
-
-export default onClickOutside(HeaderModalLeft);
