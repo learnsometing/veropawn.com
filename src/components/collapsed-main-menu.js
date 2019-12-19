c3b401f0e8ff36a4946de0264b6d6d5eca863d91
Brian Monaccio <brianmonaccio@gmail.com>
2019-12-18 20:11:21 -0500

    Removes unnecessary DDMenuList component and replaces DDMenuList reference in CollapsedMainMenu

--- src/components/collapsed-main-menu.js
+++ src/components/collapsed-main-menu.js
@@ -9,25 +9,19 @@ import React from "react"
 
 // Internal Imports
 import DDMenuBtn from "./dd-menu-btn"
-import DDMenuList from "./dd-menu-list"
 import ddMenuStyles from "./dropdown-menu.module.css"
 import MDPageLinks from "./md-page-links"
 
 export default (props) => {
   return (
-    <DDMenuList
-      children={
-        <>
-          <DDMenuBtn
-            children="Browse By Category"
-            className={ddMenuStyles.link}
-            key="browse-by-category"
-            onClick={props.openBrowseMenu}
-          />
-          <MDPageLinks />
-        </>
-      }
-      setDropdownMenuRef={props.setDropdownMenuRef}
-    />
+    <ul className={ddMenuStyles.ulist}>
+      <DDMenuBtn
+        children="Browse By Category"
+        className={ddMenuStyles.link}
+        key="browse-by-category"
+        onClick={props.openBrowseMenu}
+      />
+      <MDPageLinks />
+    </ul >
   );
 }
