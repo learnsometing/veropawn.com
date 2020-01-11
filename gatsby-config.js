574e4b916f9827684fcd73382013cdde83b1a32b
Brian Monaccio <brianmonaccio@gmail.com>
2020-01-10 23:02:48 -0500

    Reorganizes images directory, imports test images, and removes unneeded Image component

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `logos-and-icons`,
-        path: `${__dirname}/src/logos-and-icons/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `invData`,
         path: `${__dirname}/src/inv-data/`,
       },
@@ -47,7 +40,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logos-and-icons/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
