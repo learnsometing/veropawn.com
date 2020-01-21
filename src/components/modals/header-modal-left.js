05a373182ffda7adfb5e819b63b7e6662c78b99b
Brian Monaccio <brianmonaccio@gmail.com>
2020-01-21 17:52:55 -0500

    Fixes HeaderModalLeft onClickOutside and makes small style adjustments

--- src/components/modals/header-modal-left.js
+++ src/components/modals/header-modal-left.js
@@ -7,12 +7,25 @@
 
 import React from "react";
 import ReactModal from "react-modal";
+import { IconContext } from "react-icons";
+import { MdClose } from "react-icons/md";
 
 import styles from "./header-modal-left.module.css";
+
+export const CloseButton = ({ closeModal }) => (
+  <div className={styles.closeButtonContainer}>
+    <IconContext.Provider value={{ size: '48px' }}>
+      <button onClick={closeModal} className={styles.closeButton}>
+        <MdClose />
+      </button>
+    </IconContext.Provider>
+  </div>
+);
 
 export default ({ isOpen, closeModal, children }) => {
   return (
     <ReactModal
+      ariaHideApp={false}
       isOpen={isOpen}
       onRequestClose={closeModal}
       role={'menu'}
@@ -31,6 +44,7 @@ export default ({ isOpen, closeModal, children }) => {
     >
       <div className={styles.uListWrapper}>
         <ul className={styles.uList}>
+          <CloseButton closeModal={closeModal} />
           {children}
         </ul >
       </div>
