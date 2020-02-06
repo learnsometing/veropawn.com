9224d9c79c675b20cd1b61121f07a39af1b531f3
Brian Monaccio <brianmonaccio@gmail.com>
2020-02-06 18:41:24 -0500

    Styling and layout changes on Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import sizeMe from 'react-sizeme';
 import { MdPhone } from 'react-icons/md';
-import { FaDirections } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 import SEO from '../components/seo';
 import { Layout } from '../components/layout/layout';
@@ -26,52 +25,33 @@ function SectionHeader({ text }) {
     </h2>
   );
 }
-
 function Directions() {
   return (
-    <div className={`${layout.columnStartStart} ${contact.info}`}>
-      <div>
-        <div className={contact.store}>
-          Cash Pawn & Jewelry
-        </div>
-        <address className={`${layout.rowStartCenter} ${contact.address}`}>
-          854 21st St <br></br>
-          Vero Beach, FL 32960
-        </address>
-        <button
-          className={`${layout.rowStartCenter} ${contact.btn}`}
-          onClick={selectMapsApp}
-        >
-          <IconContext.Provider value={{
-            size: '1em',
-            style: {
-              marginRight: '0.5rem',
-            },
-            className: contact.icon
-          }}>
-            <FaDirections />
-          </IconContext.Provider>
-          Directions
-        </button>
-      </div >
+    <div className={layout.columnStartStart}>
+      <div className={contact.store}>
+        Cash Pawn & Jewelry
+      </div>
+      <address className={contact.address}>
+        854 21st St Vero Beach,<br></br> FL 32960
+      </address>
     </div>
   );
 
-  function selectMapsApp() {
-    if /* if we're on iOS, open in Apple Maps */
-      ((navigator.platform.indexOf("iPhone") !== -1) ||
-      (navigator.platform.indexOf("iPad") !== -1) ||
-      (navigator.platform.indexOf("iPod") !== -1)) {
-      window.open("maps://maps.google.com/maps?daddr=27.639995,-80.3904946&amp;ll=");
-    } else /* else use Google */ {
-      window.open("https://maps.google.com/maps?daddr=27.639995,-80.3904946&amp;ll=");
-    }
-  }
+  // function selectMapsApp() {
+  //   if /* if we're on iOS, open in Apple Maps */
+  //     ((navigator.platform.indexOf("iPhone") !== -1) ||
+  //     (navigator.platform.indexOf("iPad") !== -1) ||
+  //     (navigator.platform.indexOf("iPod") !== -1)) {
+  //     window.open("maps://maps.google.com/maps?daddr=27.639995,-80.3904946&amp;ll=");
+  //   } else /* else use Google */ {
+  //     window.open("https://maps.google.com/maps?daddr=27.639995,-80.3904946&amp;ll=");
+  //   }
+  // }
 }
 
 function Call() {
   return (
-    <a href="tel:7722995626" className={`${layout.rowStartCenter} ${contact.btn}`}>
+    <a href="tel:7722995626" className={`${layout.rowStartCenter}`}>
       <IconContext.Provider value={{ className: contact.icon }}>
         <MdPhone />
       </IconContext.Provider>
@@ -125,7 +105,7 @@ function Contact({ size }) {
       <SEO title='Contact' />
       <main className={contact.wrapper}>
         <ContactHeader />
-        <main className={contact.layout}>
+        <main className={`${contact.layout} ${layout.columnStartCenter}`}>
           <div className={`${layout.columnStartStart} ${contact.infoWrapper}`}>
             {/* Directions */}
             <section className={contact.section}>
@@ -146,7 +126,6 @@ function Contact({ size }) {
           {/* Google Maps */}
           <Map />
         </main>
-        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7069.0002050645035!2d-80.390495!3d27.639995!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x21e3c9b24b5b4085!2sCash%20Pawn%20%26%20Jewelry!5e0!3m2!1sen!2sus!4v1580428945177!5m2!1sen!2sus" width="1024" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe> */}
       </main>
     </Layout >
   );
