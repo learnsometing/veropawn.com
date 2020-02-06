61ad39db910a57ddf01c72458a21c6f683dff6c5
Brian Monaccio <brianmonaccio@gmail.com>
2020-02-06 10:17:01 -0500

    Moves TimedCarousel features to Carousel
    Adds slideStyle and currentSlideStyle props to allow overriding default
    class granted styles.

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,168 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
-import { FaAngleLeft, FaAngleRight, FaDollarSign } from 'react-icons/fa';
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { MdFullscreen, MdFullscreenExit } from "react-icons/md";
-
+import ReactHtmlParser from 'react-html-parser';
 import { IconContext } from 'react-icons';
 import uniqueId from 'lodash/uniqueId';
 
-import carousel from './carousel.module.scss';
-import layout from '../../styles/layout.module.css';
-
+import Cues from './cues';
 import { useCurrentIndex } from './hooks/useCurrentIndex';
 
-export const Cue = ({ isActive, isFullScreen }) => {
-  var color = getColor(isActive, isFullScreen);
-
-  return (
-    <IconContext.Provider
-      value={{
-        className: carousel.cue,
-        color: `rgba(${color})`,
-      }}
-    >
-      <FaDollarSign data-testid="carousel-pos-indicator" />
-    </IconContext.Provider>
-  );
-
-  function getColor(isActive, isFullScreen) {
-    let alpha, color;
-    if (isActive) {
-      alpha = '0.8';
-    } else {
-      alpha = '0.4';
-    }
-
-    if (isFullScreen) {
-      color = '255, 255, 255';
-    } else {
-      color = '0, 0, 0';
-    }
-
-    return `${color}, ${alpha}`;
-  }
-};
-
-Cue.propTypes = {
-  isActive: PropTypes.bool.isRequired,
-  isFullScreen: PropTypes.bool.isRequired,
-};
-
-export const Cues = ({ currentIndex, isFullScreen, length }) => {
-  var cues = createCues(length);
-
-  return (
-    <div className={`${layout.rowCenterCenter} ${carousel.cues}`}>
-      {cues}
-    </div>
-  );
-
-  function createCues(length) {
-    let range = [...Array(length).keys()];
-
-    return range.map(n => {
-      let isActive = n === currentIndex;
-      return (
-        <Cue
-          key={n}
-          isActive={isActive}
-          isFullScreen={isFullScreen}
-        />
-      );
-    })
-  }
-};
-
-Cues.propTypes = {
-  currentIndex: PropTypes.number.isRequired,
-  isFullScreen: PropTypes.bool.isRequired,
-  length: PropTypes.number.isRequired,
-};
-
-Cues.defaultProps = {
-  isFullScreen: false,
-};
-
-export function Slide({ slide, className }) {
-  return (
-    <Img
-      alt={slide.alt}
-      fluid={slide.photo.childImageSharp.fluid}
-      loading={'eager'}
-      className={className}
-    />
-  );
-}
-
-Slide.propTypes = {
-  slide: PropTypes.object.isRequired,
-  className: PropTypes.string.isRequired,
-};
-
-export const Slides = ({ content, isDisabled, visibleRange }) => {
-  // Make a list of all the slides
-  var slides = createSlides();
-
-  // Only display slides in the visible range (prev, current, next)
-  return (
-    <div className={carousel.slideContainer}>
-      {slides}
-    </div>
-  );
-
-  function createSlides() {
-    // return either a single slide (one photo), or multiple slides
-    let _slides;
-
-    if (isDisabled) {
-      // isDisabled if content.length < 2, so return one slide.
-      let slide = content[0];
-      _slides = <Slide key={slide.photo.id} slide={slide} className={carousel.singleSlide} />;
-    } else {
-      _slides = visibleRange.map((n, idx) => {
-        let slide = content[n];
-        return <Slide key={getId()} slide={slide} className={getClassName()} />;
-
-        function getClassName() {
-          let _className;
-
-          if (idx === 1) {
-            _className = carousel.currentSlide;
-          } else {
-            _className = carousel.slide;
-          }
-
-          return _className;
-        }
-
-        function getId() {
-          /*
-          * Generate a key for the slide. When only two slides are in the 
-          * carousel, the data id can't be used, because the id will be repeated
-          * on the next and previous slide, causing duplication errors.
-          */
-
-          let id;
-          if (content.length === 2) {
-            id = uniqueId();
-          } else {
-            id = slide.photo.id;
-          }
-          return id;
-        }
-      });
-    }
-
-    return _slides;
-  }
-};
-
-Slides.propTypes = {
-  content: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isDisabled: PropTypes.bool.isRequired,
-  visibleRange: PropTypes.arrayOf(PropTypes.number).isRequired,
-};
+import carousel from './carousel.module.scss';
+import layout from '../../styles/layout.module.css';
 
 export const CarouselControl = ({ children, name, onClick }) => (
   <button
@@ -190,30 +39,160 @@ export const NextPrevBtns = ({ isDisabled, onClick }) => {
   }
 
   return (
-    <div className={`${layout.rowSpaceBtnCenter} ${carousel.controlContainer}`}>
-      <IconContext.Provider
-        value={{
-          color: 'hsla(255, 255%, 255%, 0.8)',
-          size: '1.5em'
-        }}
-      >
-        <div className={`${layout.rowSpaceBtnCenter} ${carousel.controls}`}>
-          <CarouselControl
-            name="prev"
-            onClick={onClick}
-          >
-            <FaAngleLeft data-testid="fa-angle-left-icon" />
-          </CarouselControl>
-          <CarouselControl
-            name="next"
-            onClick={onClick}
-          >
-            <FaAngleRight data-testid="fa-angle-right-icon" />
-          </CarouselControl>
-        </div>
-      </IconContext.Provider>
+    <IconContext.Provider
+      value={{
+        color: 'hsla(255, 255%, 255%, 0.8)',
+        size: '1.5em'
+      }}
+    >
+      <div className={`${layout.rowSpaceBtnCenter} ${carousel.controls}`}>
+        <CarouselControl
+          name="prev"
+          onClick={onClick}
+        >
+          <FaAngleLeft data-testid="fa-angle-left-icon" />
+        </CarouselControl>
+        <CarouselControl
+          name="next"
+          onClick={onClick}
+        >
+          <FaAngleRight data-testid="fa-angle-right-icon" />
+        </CarouselControl>
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+export function Slide({ slide, className, style }) {
+  return (
+    <div className={className} style={style}>
+      <Img
+        alt={slide.alt}
+        fluid={slide.photo.childImageSharp.fluid}
+        loading={'eager'}
+        className={carousel.img}
+      />
+      <TextOverlay content={slide} />
     </div>
   );
+}
+
+Slide.propTypes = {
+  slide: PropTypes.object.isRequired,
+  className: PropTypes.string.isRequired,
+  style: PropTypes.object.isRequired,
+};
+
+function TextOverlay({ content }) {
+  var html = content.html;
+  if (html) {
+    let title = content.title;
+    return (
+      <div className={carousel.textOverlayWrapper}>
+        <div className={`${layout.columnStartCenter} ${carousel.textOverlay}`}>
+          <p className={carousel.title}>{title}</p>
+          {ReactHtmlParser(html)}
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+}
+
+export const Slides = (props) => {
+  var {
+    content,
+    currentSlideStyle,
+    isDisabled,
+    onClick,
+    slideStyle,
+    visibleRange
+  } = props;
+  // Make a list of all the slides
+  var slides = createSlides();
+
+  // Only display slides in the visible range (prev, current, next)
+  return (
+    <div className={carousel.slideContainer}>
+      {slides}
+      <NextPrevBtns
+        isDisabled={isDisabled}
+        onClick={onClick}
+      />
+    </div>
+  );
+
+  function createSlides() {
+    // return either a single slide (one photo), or multiple slides
+    let _slides;
+
+    if (isDisabled) {
+      // isDisabled if content.length < 2, so return one slide.
+      let slide = content[0];
+      _slides = <Slide key={slide.photo.id} slide={slide} className={carousel.singleSlide} />;
+    } else {
+      _slides = visibleRange.map((n, idx) => {
+        let slide = content[n];
+        let [className, style] = getClassAndStyle();
+        let key = getId();
+
+        return (
+          <Slide
+            key={key}
+            className={className}
+            slide={slide}
+            style={style}
+          />
+        );
+
+        function getClassAndStyle() {
+          let _className;
+          let _style = undefined;
+
+          if (idx === 1) {
+            _className = carousel.currentSlide;
+            if (currentSlideStyle) {
+              _style = currentSlideStyle;
+            }
+          } else {
+            _className = carousel.slide;
+            if (slideStyle) {
+              _style = slideStyle;
+            }
+          }
+
+          return [_className, _style];
+        }
+
+        function getId() {
+          /*
+          * Generate a key for the slide. When only two slides are in the 
+          * carousel, the data id can't be used, because the id will be repeated
+          * on the next and previous slide, causing duplication errors.
+          */
+
+          let id;
+          if (content.length === 2) {
+            id = uniqueId();
+          } else {
+            id = slide.photo.id;
+          }
+          return id;
+        }
+      });
+    }
+
+    return _slides;
+  }
+};
+
+Slides.propTypes = {
+  content: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentSlideStyle: PropTypes.string,
+  isDisabled: PropTypes.bool.isRequired,
+  slideStyle: PropTypes.string,
+  visibleRange: PropTypes.arrayOf(PropTypes.number).isRequired,
 };
 
 export const FullScreenButton = ({ onClick }) => (
@@ -249,22 +228,37 @@ export function FSIcon({ isFullScreen, onClick }) {
 }
 
 const Carousel = (props) => {
-  var { content, isFullScreen, onFSIconClick, onIndexChange, startIndex } = props;
+  var {
+    content,
+    currentSlideStyle,
+    isFullScreen,
+    isTimed,
+    onFSIconClick,
+    onIndexChange,
+    slideStyle,
+    startIndex
+  } = props;
+
   var length = content.length;
   var isDisabled = length < 2;
   var currentIndex = useCurrentIndex(startIndex, onIndexChange, length);
+  useEffect(() => {
+    if (isTimed) {
+      let timerID = currentIndex.setTimer(8000);
+      return () => clearInterval(timerID);
+    }
+  }, [currentIndex, isTimed]);
 
   return (
     <div className={`${layout.columnStartCenter} ${carousel.wrapper}`}>
       <div className={`${carousel.carousel}`}>
         <Slides
           content={content}
-          isDisabled={isDisabled}
-          visibleRange={currentIndex.visibleRange}
-        />
-        <NextPrevBtns
+          currentSlideStyle={currentSlideStyle}
           isDisabled={isDisabled}
           onClick={currentIndex.onClick}
+          slideStyle={slideStyle}
+          visibleRange={currentIndex.visibleRange}
         />
       </div>
       <div className={`${layout.rowEndCenter} ${carousel.FSControlContainer}`}>
@@ -281,14 +275,18 @@ const Carousel = (props) => {
 
 Carousel.propTypes = {
   content: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentSlideStyle: PropTypes.string,
   isFullScreen: PropTypes.bool.isRequired,
+  isTimed: PropTypes.bool.isRequired,
   onFSIconClick: PropTypes.func.isRequired,
   onIndexChange: PropTypes.func.isRequired,
+  slideStyle: PropTypes.string,
   startIndex: PropTypes.number.isRequired,
 };
 
 Carousel.defaultProps = {
   isFullScreen: false,
+  isTimed: false,
   onFSIconClick: () => { },
   onIndexChange: () => { },
   startIndex: 0,
