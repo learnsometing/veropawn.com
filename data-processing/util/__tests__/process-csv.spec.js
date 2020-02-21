9079c34306ee81027d907d50cddc564dc0a0889b
Brian Monaccio <brianmonaccio@gmail.com>
2020-02-21 17:07:00 -0500

    Restructures, cleans, and adds comments to process-csv code

--- data-processing/util/__tests__/process-csv.spec.js
+++ data-processing/util/__tests__/process-csv.spec.js
@@ -1,46 +1,39 @@
 jest.mock('../parse-csv');
-const { getFkNamePairs, getInvData, resolveFile } = require("../process-csv");
-const { catFksToNames, prettifiedInvData } = require("../__mocks__/parse-csv");
-describe('getFKNamePairs', () => {
-  let data = `
+var { parseFKTable, parseInvTable, parseRecords } = require("../process-csv");
+var { catFksToNames, prettifiedInvData } = require("../__mocks__/parse-csv");
+
+let fkData = `
     fk      name
     4001    COINS
     8001    FIREARM
     12001   JEWELRY
     21001   TOOLS-POWER
-  `
+  `;
 
-  it('should return a promise resolved to a map of parsed csv data', () => {
-    return expect(getFkNamePairs(data)).resolves.toEqual(catFksToNames);
-  });
-});
-
-describe('getInvData', () => {
-  let data = `
+let invData = `
   12001   2339001 LADIES RING W/STONES    RING JEWELRY NONE, 14KT, 4.80 Grams; LADIES RING W/STONES               I-435
   21001   3299001 PHONE LINE TESTER       TESTER TOOLS-POWER FLUKE TS19; PHONE LINE TESTER        TS19    32825-5
   12001   2339001 MANS  RING      RING JEWELRY NONE, 14KT, 11.00 Grams; MANS  RING                I-942
-  `
+  `;
+
+describe('parseFKTable', () => {
+  it('should return a promise resolved to a map of parsed csv data', () => {
+    return expect(parseFKTable(fkData)).resolves.toEqual(catFksToNames);
+  });
+});
+
+describe('parseInvTable', () => {
   it('should return the prettified, parsed inventory CSV', () => {
-    return expect(getInvData(data)).resolves.toEqual(prettifiedInvData);
+    return expect(parseInvTable(invData)).resolves.toEqual(prettifiedInvData);
   });
 })
 
-describe('resolveFile', () => {
+describe('parseRecords', () => {
   it('should resolve correctly when given the path to the inventory table', () => {
-    let p = new Promise((resolve, reject) => resolveFile('inv.tsv', '', resolve));
-    return expect(p).resolves.toEqual(prettifiedInvData);
+    return expect(parseRecords('inv.tsv', invData)).resolves.toEqual(prettifiedInvData);
   });
 
   it('should resolve correctly when given the path to the category fk table', () => {
-    const data = `
-    fk      name
-    4001    COINS
-    8001    FIREARM
-    12001   JEWELRY
-    21001   TOOLS-POWER
-  `
-    let p = new Promise((resolve, reject) => resolveFile('category.tsv', data, resolve));
-    return expect(p).resolves.toEqual(catFksToNames);
+    return expect(parseRecords('category.tsv', fkData)).resolves.toEqual(catFksToNames);
   });
 });
--- data-processing/util/process-csv.js
+++ data-processing/util/process-csv.js
@@ -1,12 +1,28 @@
-const fs = require('fs');
-const { parseCSV } = require('./parse-csv');
-const { prettifyHeaders } = require('./text-formatting');
+var fs = require('fs');
+var { parseCSV } = require('./parse-csv');
+var { prettifyHeaders } = require('./text-formatting');
 
-async function getFkNamePairs(data) {
+function processedCSV(path, resolve, reject) {
+  // read in the .tsv file
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+    }
+
+    // get the processed records that were read into memory
+    let records = parseRecords(path, data);
+    resolve(records);
+  });
+};
+
+exports.processedCSV = processedCSV;
+
+async function parseFKTable(data) {
   /*
-  * returns a promise resolved with either a map of foreign key, name pairs or 
+  * returns a promise resolved with either a map of foreign key/name pairs or
   * an error generated during CSV parsing.
   */
+
   let map = new Map();
   let parsedCSV = await parseCSV(data, {
     separator: '\t',
@@ -18,46 +34,38 @@ async function getFkNamePairs(data) {
   return map;
 };
 
-exports.getFkNamePairs = getFkNamePairs;
+exports.parseFKTable = parseFKTable;
 
-async function getInvData(data) {
+async function parseInvTable(data) {
   /*
-  * returns an array of records as objects from the inventory data table. 
+  * returns a promise resolved with either an array of record objects or
+  * an error generated during CSV parsing.
   * 
   * Prettifies each record's header name to adhere to JS camelCase style.
   */
 
   let parsedCSV = await parseCSV(data, {
+    // method from neat-csv module, applies prettifyHeaders to each header
     mapHeaders: ({ header, index }) => prettifyHeaders(header),
+    // methods from neat-csv module, trims each value
     mapValues: ({ header, index, value }) => value.trim(),
     separator: '\t',
     skipComments: true,
+    strict: false
   });
 
   return parsedCSV;
 };
 
-exports.getInvData = getInvData;
+exports.parseInvTable = parseInvTable;
 
-const resolveFile = (_path, _data, _resolve) => {
-  if (_path.includes('inv')) {
-    _resolve(getInvData(_data));
-  } else if (_path.includes('category')) {
-    // both foreign key tables include the word category
-    _resolve(getFkNamePairs(_data));
+function parseRecords(path, data) {
+  // decide how to process the file based on its path
+  if (path.includes('inv')) {
+    return parseInvTable(data);
+  } else if (path.includes('category')) {
+    return parseFKTable(data);
   }
 };
 
-exports.resolveFile = resolveFile;
-
-const processedCSV = (_path, _resolve, _reject) => {
-  fs.readFile(_path, 'utf8', (_err, _data) => {
-    if (_err) {
-      _reject(_err);
-    }
-
-    resolveFile(_path, _data, _resolve);
-  });
-};
-
-exports.processedCSV = processedCSV;
+exports.parseRecords = parseRecords;
