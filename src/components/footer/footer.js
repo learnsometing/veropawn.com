5ad3f18e20e0143da5b5bd14b910e98836bc2aa9
Brian Monaccio <brianmonaccio@gmail.com>
2020-02-23 18:57:28 -0500

    Cleans and adds comments to footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,26 +5,28 @@ import { IconContext } from "react-icons";
 import footer from "./footer.module.css";
 import layout from "../../styles/layout.module.css";
 
-export default () => {
-  const socialIconsClass = `${layout.rowCenterCenter} ${footer.socialIcons}`;
-  const socialIconClass = `${layout.rowCenterCenter} ${footer.socialIcon}`;
-  const copyrightContainerClass = `${layout.rowCenterCenter} ${footer.copyrightContainer}`;
-
-  return (
-    <footer className={`${layout.columnCenterCenter} ${footer.footer}`}>
-      <div className={footer.wrapper}>
-        <div className={socialIconsClass}>
-          <IconContext.Provider value={{ color: 'rgba(54, 115, 63, 1)', size: '48px' }}>
-            <a className={socialIconClass} href="https://www.ebay.com/usr/cashpawn"><FaEbay /></a>
-            <a className={socialIconClass} href="https://www.facebook.com/VBPawn"><FaFacebookSquare /></a>
-          </IconContext.Provider>
-        </div>
-        <div className={copyrightContainerClass}>
-          <span >
-            &#169;{new Date().getFullYear()} Cash Pawn and Jewelry, Vero Beach, FL
-        </span>
-        </div>
+export default () => (
+  <footer className={`${layout.columnCenterCenter} ${footer.footer}`}>
+    <div className={footer.wrapper}>
+      <div className={`${layout.rowCenterCenter} ${footer.socialIcons}`}>
+        <IconContext.Provider value={{ color: 'rgba(54, 115, 63, 1)', size: '48px' }}>
+          <a
+            className={`${layout.rowCenterCenter} ${footer.socialIcon}`}
+            href="https://www.ebay.com/usr/cashpawn">
+            <FaEbay />
+          </a>
+          <a
+            className={`${layout.rowCenterCenter} ${footer.socialIcon}`}
+            href="https://www.facebook.com/VBPawn">
+            <FaFacebookSquare />
+          </a>
+        </IconContext.Provider>
       </div>
-    </footer>
-  );
-};
+      <div className={`${layout.rowCenterCenter} ${footer.copyrightContainer}`}>
+        <span >
+          &#169;{new Date().getFullYear()} Cash Pawn and Jewelry, Vero Beach, FL
+        </span>
+      </div>
+    </div>
+  </footer>
+);
